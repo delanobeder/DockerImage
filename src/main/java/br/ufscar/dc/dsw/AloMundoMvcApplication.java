0ea3c6b6cf537f46753f1783100b69c60ae25b3e
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2025-05-29 15:16:21 -0300

    Update AloMundoMvcApplication.java

--- src/main/java/br/ufscar/dc/dsw/AloMundoMvcApplication.java
+++ src/main/java/br/ufscar/dc/dsw/AloMundoMvcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AloMundoMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AloMundoMvcApplication.class, args);
+		SpringApplication.run(AloMundoMvcApplication.class, args)
 	}
 
 }
